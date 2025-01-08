01595fc42e5b70f0a818debe724b180f6b86c730
Artemiy <artemstickly@icloud.com>
2025-01-08 19:40:19 +0400

    button

--- main.py
+++ main.py
@@ -88,24 +88,49 @@ class TextView(ft.ListView):
 
     def on_hover(self, e, row):
         if e.data == 'true':
-            row.bgcolor = ft.colors.BLACK38
-            row.update()
+            if self.selected_row is not row:
+                row.bgcolor = ft.colors.BLACK38
         else:
-            row.bgcolor = ft.colors.TRANSPARENT
-            row.update()
+            if self.selected_row is not row:
+                row.bgcolor = ft.colors.TRANSPARENT
+        row.update()
+
+    def get_selected_ip(self):
+        if self.selected_row is not None:
+            index = self.controls.index(self.selected_row)
+            return self.persons[index]['ip']
+        return None
 
 
 def main(page: ft.Page):
+    #page.theme_mode = ft.ThemeMode.DARK
+    page.window_width = 600
+    page.window_height = 400
+    page.window_center()
+    page.title = 'UPF List'
     button_disk = BaseButton(
         icon=ft.icons.FOLDER_SHARED,
     )
     text_view = TextView(persons, button_disk)
     text_search = TextSearch(persons, text_view)
+
+    def on_keyboard_button_click(e):
+        ip = text_view.get_selected_ip()
+        if ip:
+            page.add(ft.Text(f'Connecting to {ip}'))
+
+    def on_connect_button_click(e):
+        ip = text_view.get_selected_ip()
+        if ip:
+            page.add(ft.Text(f'Connecting to {ip}'))
+
     connect_button = BaseButton(
         icon=ft.icons.CONNECTED_TV,
+        on_click=on_connect_button_click
     )
     keyboard_button = BaseButton(
-        icon=ft.icons.KEYBOARD
+        icon=ft.icons.KEYBOARD,
+        on_click=on_keyboard_button_click
     )
     page.add(
         ft.Row(
